da4785609ccccd448baed3c25e81daa5bfdecea1
Everistus Olumese <everistusemail@gmail.com>
2019-09-07 13:34:13 +0100

    Adding

--- gke/index.js
+++ gke/index.js
@@ -6,4 +6,9 @@ app.get('/', (req, res) => {
     res.json({ success: true, message: 'We have successfully deployed our application on kubernetes engine' })
 })
 
+
+app.get('/v2', (req, res) => {
+    res.json({ success: true, message: 'We have successfully deployed our version 2 of our application on kubernetes engine' })
+})
+
 app.listen(PORT, () => console.log('Application running on port ', PORT))
